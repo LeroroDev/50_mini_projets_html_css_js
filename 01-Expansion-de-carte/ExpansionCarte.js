0afb422e579f0dea911e9e13f4fd1db636c50627
Ronny <lambeauronny@gmail.com>
2023-02-15 13:32:53 +0100

    passage a jquery

--- 01-Expansion-de-carte/ExpansionCarte.js
+++ 01-Expansion-de-carte/ExpansionCarte.js
@@ -1,14 +1,10 @@
-const cards = document.querySelectorAll('.card')
+const $cards = $('.card');
 
-cards.forEach(card => {
-    card.addEventListener('click', () => {
-        removeActiveClasses()
-        card.classList.add('active')
-    })
-})
+$cards.on('click', function() {
+    removeActiveClasses();
+    $(this).addClass('active');
+});
 
 function removeActiveClasses() {
-    cards.forEach(card => {
-        card.classList.remove('active')
-    })
+    $cards.removeClass('active');
 }
